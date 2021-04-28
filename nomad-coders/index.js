1e00c3e0b30fdbae1e510d42f75408af47744870
eunbin-lee <euns2bin@gmail.com>
2021-04-28 18:42:58 +0900

    Nomad Coders | First steps with TypeScript

--- nomad-coders/index.js
+++ nomad-coders/index.js
@@ -1,2 +1,9 @@
-alert('hello');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = 'Nicolas', age = 24, gender = 'male';
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+sayHi(name, age);
+// gender? : gender 파라미터는 옵션임을 의미
 //# sourceMappingURL=index.js.map
--- nomad-coders/index.ts
+++ nomad-coders/index.ts
@@ -1,1 +1,13 @@
-alert('hello');
+const name = 'Nicolas',
+  age = 24,
+  gender = 'male';
+
+const sayHi = (name, age, gender?) => {
+  console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+
+sayHi(name, age);
+
+export {};
+
+// gender? : gender 파라미터는 옵션임을 의미
