73d2e6636f066514b6ad6b809784a37fe8cc91fe
eunbin-lee <euns2bin@gmail.com>
2021-05-02 09:25:20 +0900

    Nomad Coders | Creating a Block 1

--- nomad-coders/src/index.ts
+++ nomad-coders/src/index.ts
@@ -1,26 +1,28 @@
-class Human {
-  public name: string;
-  public age: number;
-  public gender: string;
-  constructor(name: string, age: number, gender: string) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number,
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
   }
 }
 
-const lynn = new Human('Lynn', 18, 'female');
+const genesisBlock: Block = new Block(0, '202020202020', '', 'Hello', 123456);
 
-const sayHi = (person: Human): string => {
-  return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
-};
+let blockchain: [Block] = [genesisBlock];
 
-console.log(sayHi(lynn));
+console.log(blockchain);
 
 export {};
-
-/* 
-interface는 js로 컴파일 되지 않음 → js로 컴파일 시켜주고 싶다면 class를 사용
-- public: 밖에서 접근할 수 있는 속성
-- private: 밖에서 접근할 수 없는 속성
-*/
