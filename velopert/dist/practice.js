497dd135becd406a56f07b4e6bdfdc7b5d17e9b6
eunbin-lee <euns2bin@gmail.com>
2021-05-19 10:21:40 +0900

    velopert | Interface, Type Alias

--- velopert/dist/practice.js
+++ velopert/dist/practice.js
@@ -1,29 +1,25 @@
 "use strict";
-let count = 0;
-count += 1;
-// count = '문자열';
-const message = 'hello world';
-const done = false;
-const numbers = [1, 2, 3];
-const messages = ['hello', 'world'];
-// messages.push(1);
-let mightBeUndefined = undefined;
-let nullableNumber = null;
-let color = 'red';
-color = 'yellow';
-// color = 'green';
-function sum(x, y) {
-    return x + y;
+// implements: Circle은 Shape이 가진 조건들을 충족시킴을 의미
+class Circle {
+    constructor(radius) {
+        this.radius = radius;
+    }
+    getArea() {
+        return this.radius * this.radius * Math.PI;
+    }
 }
-const result = sum(1, 2);
-function sumArray(numbers) {
-    return numbers.reduce((acc, cur) => acc + cur, 0);
+class Rectangle {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+    getArea() {
+        return this.width * this.height;
+    }
 }
-const total = sumArray([1, 2, 3, 4, 5]);
-console.log(total);
-function returnNothing() {
-    console.log('어쩌고저쩌고');
-}
-function returnStringOrNumber() {
-    return 4;
-}
+const circle = new Circle(5);
+const rectangle = new Rectangle(2, 5);
+const shapes = [circle, rectangle];
+shapes.forEach((shape) => {
+    console.log(shape.getArea());
+});
--- velopert/src/practice.ts
+++ velopert/src/practice.ts
@@ -1,36 +1,75 @@
-let count = 0;
-count += 1;
-// count = '문자열';
-
-const message: string = 'hello world';
-const done: boolean = false;
-
-const numbers: number[] = [1, 2, 3];
-const messages: string[] = ['hello', 'world'];
-// messages.push(1);
-
-let mightBeUndefined: string | undefined = undefined;
-let nullableNumber: number | null = null;
-
-let color: 'red' | 'orange' | 'yellow' = 'red';
-color = 'yellow';
-// color = 'green';
-
-function sum(x: number, y: number): number {
-  return x + y;
+// [ Interface ] - 클래스 또는 객체를 위한 타입을 지정할 때 사용
+interface Shape {
+  getArea(): number;
 }
-const result = sum(1, 2);
+// implements: Circle은 Shape이 가진 조건들을 충족시킴을 의미
+class Circle implements Shape {
+  radius: number;
 
-function sumArray(numbers: number[]): number {
-  return numbers.reduce((acc, cur) => acc + cur, 0);
-}
-const total = sumArray([1, 2, 3, 4, 5]);
-console.log(total);
+  constructor(radius: number) {
+    this.radius = radius;
+  }
 
-function returnNothing(): void {
-  console.log('어쩌고저쩌고');
+  getArea() {
+    return this.radius * this.radius * Math.PI;
+  }
 }
 
-function returnStringOrNumber(): string | number {
-  return 4;
+class Rectangle implements Shape {
+  constructor(public width: number, private height: number) {}
+
+  getArea() {
+    return this.width * this.height;
+  }
 }
+
+const circle: Circle = new Circle(5);
+const rectangle = new Rectangle(2, 5);
+const shapes: Shape[] = [circle, rectangle];
+
+shapes.forEach((shape) => {
+  console.log(shape.getArea());
+});
+
+// [ Type Alias ]
+/*
+interface Person  {
+  name: string;
+  age?: number;
+}
+*/
+type Person = {
+  name: string;
+  age?: number;
+};
+
+// extends (= Person &): Developert는 Person을 상속받음
+/*
+interface Developer extends Person {
+  skills: string[];
+}
+*/
+type Developer = Person & {
+  skills: string[];
+};
+
+const person: Person = {
+  name: '김사람',
+  age: 20,
+};
+
+const expert: Developer = {
+  name: '김개발',
+  skills: ['javascript', 'react', 'typescript'],
+};
+
+type People1 = Person[];
+const people: People1 = [person, expert];
+
+type Color = 'red' | 'orange' | 'yellow';
+const color: Color = 'orange';
+
+/*
+어떤 라이브러리를 위한 타입을 설정하는 경우에는 interface만 사용하는 것을 권장
+interface와 type alias 중 하나만 사용하는 것을 권장
+*/
