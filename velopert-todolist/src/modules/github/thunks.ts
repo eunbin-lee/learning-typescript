5a5b8fe1f1f8e22dd5d7db5f90d01d8497ba0a81
eunbin-lee <euns2bin@gmail.com>
2021-05-28 18:13:44 +0900

    velopert | 타입스크립트와 리덕스 미들웨어 사용하기: thunk, 리듀서 리팩토링

--- velopert-todolist/src/modules/github/thunks.ts
+++ velopert-todolist/src/modules/github/thunks.ts
@@ -1,18 +1,21 @@
-import { Dispatch } from 'redux';
-import { getUserProfile } from '../../api/github';
 import { getUserProfileAsync } from './actions';
+import { getUserProfile } from '../../api/github';
+import createAsyncThunk from '../../lib/createAsyncThunk';
 
-export function getUserProfileThunk(username: string) {
-  return async (dispatch: Dispatch) => {
-    const { request, success, failure } = getUserProfileAsync;
-    dispatch(request());
-    try {
-      const userProfile = await getUserProfile(username);
-      dispatch(success(userProfile));
-    } catch (e) {
-      dispatch(failure(e));
-    }
-  };
-}
+// export function getUserProfileThunk(username: string) {
+//   return async (dispatch: Dispatch) => {
+//     const { request, success, failure } = getUserProfileAsync;
+//     dispatch(request());
+//     try {
+//       const userProfile = await getUserProfile(username);
+//       dispatch(success(userProfile));
+//     } catch (e) {
+//       dispatch(failure(e));
+//     }
+//   };
+// }
 
-// = export function getUserProfileThunk(username: string): ThunkAction<return 값, root 상태, extraArgument 타입, actions 타입> {}
+export const getUserProfileThunk = createAsyncThunk(
+  getUserProfileAsync,
+  getUserProfile,
+);
